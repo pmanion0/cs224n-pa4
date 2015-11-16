b7bd4a2398e504d8d307ac98ff0eaee23ac2e61b
Patrick Manion <patrickjmanion@gmail.com>
2015-11-15 19:19:40 -0600

    Generalizing NER main class with command line arguments

--- java/src/cs224n/deep/NER.java
+++ java/src/cs224n/deep/NER.java
@@ -5,33 +5,72 @@ import java.io.*;
 
 import org.ejml.simple.SimpleMatrix;
 
+import cs224n.util.CommandLineUtils;
 
 public class NER {
+
+  public static void main(String[] args) throws IOException {
+    // Define the default option set and override with any command line args
+    Map<String, String> options = new HashMap<String, String>();
+    options.put("-train",    "../data/train");
+    options.put("-test",     "../data/dev");
+    options.put("-outfile",  "../scored.out");
+    options.put("-wordvec",  "../data/wordVectors.txt");
+    options.put("-vocab",    "../data/vocab.txt");
+    options.put("-model",    "cs224n.deep.WindowModel");
+    options.putAll(CommandLineUtils.simpleCommandLineParser(args));
+    printOptions(options);
+
+    // Convert the model name into an instance of the specified class
+    NERModel model = getModelClass(options.get("-model"));
+    System.out.println("Using parser: " + model);
+
+    // Read in the requested data files and initialize necessary data structures
+    List<Datum> trainData = FeatureFactory.readTrainData(options.get("-train"));
+    List<Datum> testData = FeatureFactory.readTestData(options.get("-test"));
     
-    public static void main(String[] args) throws IOException {
-	if (args.length < 2) {
-	    System.out.println("USAGE: java -cp classes NER ../data/train ../data/dev");
-	    return;
-	}	    
-
-	// this reads in the train and test datasets
-//	List<Datum> trainData = FeatureFactory.readTrainData(args[0]);
-//	List<Datum> testData = FeatureFactory.readTestData(args[1]);	
-	List<Datum> trainData = FeatureFactory.readTrainData("/Users/ldi020/Playground/cs224n/pa4/data/train");
-	List<Datum> testData = FeatureFactory.readTestData("/Users/ldi020/Playground/cs224n/pa4/data/dev");	
-	//	read the train and test data
-	//TODO: Implement this function (just reads in vocab and word vectors)
-	FeatureFactory.initializeVocab("/Users/ldi020/Playground/cs224n/pa4/data/vocab.txt");
-	SimpleMatrix allVecs= FeatureFactory.readWordVectors("/Users/ldi020/Playground/cs224n/pa4/data/wordVectors.txt");
-
-	// initialize model 
-	WindowModel model = new WindowModel(5, 100,0.001);
-	model.initWeights();
-
-	//TODO: Implement those two functions
-	model.trainBaseLine(trainData);
-	model.testBaseLine(testData, "/Users/ldi020/Playground/cs224n/pa4/data/output/baseline.txt");
-	//model.train(trainData);
-	//model.test(testData);
+    // Run setup for the requested model
+    if (model instanceof WindowModel) {
+      // This is awful - why is this in the NER class???
+      //SimpleMatrix allVecs = FeatureFactory.readWordVectors(options.get("-wordvec"));
+      //FeatureFactory.initializeVocab(options.get("-vocab"));
+    } else if (model instanceof BaselineModel) {
+      // Do something?
+    } else {
+      // Do something?
     }
+    
+    model.train(trainData);
+    System.out.println("Training Complete.");
+    
+    model.test(testData, options.get("-outfile"));
+    System.out.println("Testing Complete.");
+  }
+  
+  
+  /**
+   * Print all the runtime options from the options map
+   */
+  public static void printOptions(Map<String, String> options) {
+    System.out.println("NER Run Parameters:");
+    for (Map.Entry<String, String> entry: options.entrySet()) {
+      System.out.printf("  %-12s: %s%n", entry.getKey(), entry.getValue());
+    }
+    System.out.println();
+  }
+  
+  
+  /**
+   * Convert the modelName string into an instance of the model class
+   */
+  public static NERModel getModelClass(String modelName) {
+    NERModel model;
+    try {
+      Class<?> modelClass = Class.forName(modelName);
+      model = (NERModel) modelClass.newInstance();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return model;
+  }
 }
