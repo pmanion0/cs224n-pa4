b88ae5948516b3bb2cef96bdf1a54233e1300d2d
Patrick Manion <patrickjmanion@gmail.com>
2015-11-15 23:15:42 -0600

    Adding WIP changes to WindowModel

--- java/src/cs224n/deep/WindowModel.java
+++ java/src/cs224n/deep/WindowModel.java
@@ -1,37 +1,42 @@
 package cs224n.deep;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.ejml.simple.SimpleMatrix;
 
+import cs224n.util.WordWindow;
+
 public class WindowModel {
 
-  protected SimpleMatrix L, W, Wout;
-  //
-  public int windowSize, wordSize, hiddenSize;
+  public FeatureFactory ff;
+  public int wordVecSize, windowSize;
+  public int[] hiddenSizes;
+  public NeuralNetwork model;
 
-  public WindowModel(int _windowSize, int _hiddenSize, double _lr) {
-    // TODO
-  }
-
-  /**
-   * Initializes the weights randomly.
-   */
-  public void initWeights() {
-    // TODO
-    // initialize with bias inside as the last column
-    // W = SimpleMatrix...
-    // U for the score
-    // U = SimpleMatrix...
+  public WindowModel(int wordVecSize, int windowSize, int[] hiddenSizes, FeatureFactory ff) {
+    if (windowSize % 2 == 0) {
+      System.err.println("ERROR: I'm sorry, Dave. I can't let you use even windowSizes.");
+      windowSize = 3;
+    }
+    this.wordVecSize = wordVecSize;
+    this.windowSize = windowSize;
+    this.hiddenSizes = hiddenSizes;
+    this.ff = ff;
+    model = new NeuralNetwork(wordVecSize*windowSize, hiddenSizes, 5);
   }
 
   /**
    * Simplest SGD training
    */
-  public void train(List<Datum> _trainData) {
-    // TODO
+  public void train(List<Datum> trainData) {
+    WordWindow window = new WordWindow(trainData, windowSize);
+    
+    while(window.rollWindow()) {
+      int[] windowIDs = window.getWindowNum();
+    }
   }
-
+  
   public void test(List<Datum> testData) {
     // TODO
   }
