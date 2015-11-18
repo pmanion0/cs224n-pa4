5e61b8ab7b779c6a4b9540448b455874f8d485dc
Patrick Manion <patrickjmanion@gmail.com>
2015-11-17 19:05:57 -0600

    Moving BaselineModel to the FeatureFactory file output method

--- java/src/cs224n/deep/BaselineModel.java
+++ java/src/cs224n/deep/BaselineModel.java
@@ -31,39 +31,34 @@ public class BaselineModel implements NERModel {
 
   @Override
   public void test(List<Datum> testData, String outfile) {
-    ArrayList<String> labelList = new ArrayList<String>();
-    labelList.add("O");
-    labelList.add("LOC");
-    labelList.add("MISC");
-    labelList.add("ORG");
-    labelList.add("PER");
-    //{O, LOC, MISC, ORG, PER}
-    int size = labelList.size();
-    int randomIndx = getRandomInt(0, size);
-    
-    PrintWriter out;
-    try {
-      out =  new PrintWriter(new BufferedWriter(new FileWriter(outfile)));
-    } catch (Exception e) {
-      System.err.println("ERROR: Cannot open output file");
-      return;
-    }
+    List<String> predictions = new ArrayList<String>();
     
     for (Datum test : testData){
-      String word = test.word;
-      String labelGold = test.label;
-      String labelPredicted;
-      
-      //-DOCSTART-
-      if (!word.equals("-DOCSTART-") && trainNER.containsKey(word)) {
-        labelPredicted = trainNER.get(word);
-      } else {
-        // randomly assign
-        labelPredicted = labelList.get(randomIndx);
-      }     
-       out.println(word + "\t" + labelGold+ "\t" + labelPredicted);
-    } // end test
-    out.close();
+      String predLabel = predictDatum(test);
+      predictions.add(predLabel);
+    }
+    FeatureFactory.outputScoringToFile(testData, predictions, outfile);
+  }
+  
+  
+  /**
+   * Output the predicted entity label for a given input datum
+   * @param d - Input datum to score
+   * @return Entity label
+   */
+  public String predictDatum(Datum d) {
+    String word = d.word;
+    String labelPredicted;
+    
+    if (!word.equals("-DOCSTART-") && trainNER.containsKey(word)) {
+      labelPredicted = trainNER.get(word);
+    } else {
+      // Randomly assign label if we did not see it in training
+      int size = FeatureFactory.targetToNum.size();
+      int randomIndx = getRandomInt(0, size-1);
+      labelPredicted = FeatureFactory.numToTarget.get(randomIndx);
+    }
+    return labelPredicted;
   }
 
 }
