164af47a4bbe3871b0d29428d697f3ca82934726
Patrick Manion <patrickjmanion@gmail.com>
2015-11-24 21:15:53 -0600

    Resetting NER to work with original call

--- java/src/cs224n/deep/NER.java
+++ java/src/cs224n/deep/NER.java
@@ -1,86 +1,28 @@
 package cs224n.deep;
 
-import java.util.*;
-import java.io.*;
-
-import org.ejml.simple.SimpleMatrix;
-
-import cs224n.util.CommandLineUtils;
+import java.io.IOException;
+import java.util.List;
 
 public class NER {
   
   public static void main(String[] args) throws IOException {
-    // Define the default option set and override with any command line args
-    Map<String, String> options = new HashMap<String, String>();
-    options.put("-train",    "../data/train");
-    options.put("-test",     "../data/dev");
-    options.put("-outfile",  "../scored.out");
-    options.put("-wordvec",  "../data/wordVectors.txt");
-    options.put("-vocab",    "../data/vocab.txt");
-    options.put("-model",    "cs224n.deep.WindowModel");
-    options.putAll(CommandLineUtils.simpleCommandLineParser(args));
-    printOptions(options);
-
-    // Convert the model name into an instance of the specified class
-    System.out.println("Using Model: " + options.get("-model"));
-
-    // Read in the requested data files and initialize necessary data structures
-    List<Datum> trainData = FeatureFactory.readTrainData(options.get("-train"));
-    List<Datum> testData = FeatureFactory.readTestData(options.get("-test"));
     
-    // Run setup for the requested model
-    if (options.get("-model").equals("cs224n.deep.WindowModel")) {
-      // RUN THE WINDOWMODEL
-      WindowModel model = new WindowModel(50, 5, new int[]{20});
+    if (args.length < 2) {
+        System.out.println("USAGE: java -cp classes NER ../data/train ../data/dev");
+        return;
+    }     
+  
+    // Read in the requested data files and initialize necessary data structures
+    List<Datum> trainData = FeatureFactory.readTrainData(args[0]);
+    List<Datum> testData = FeatureFactory.readTestData(args[1]);
 
-      FeatureFactory.readWordVectors(options.get("-wordvec"));
-      FeatureFactory.initializeVocab(options.get("-vocab"));
+    // RUN THE WINDOWMODEL
+    WindowModel model = new WindowModel(50, 5, new int[]{20});
+
+    FeatureFactory.readWordVectors("../data/wordVectors.txt");
+    FeatureFactory.initializeVocab("../data/vocab.txt", "UUUNKKK");
       
-      model.train(trainData);
-      System.out.println("Training Complete.");
-      
-      model.test(testData, options.get("-outfile"));
-      System.out.println("Testing Complete.");
-    }
-    else if (options.get("-model").equals("cs224n.deep.BaselineModel")) {
-      // RUN THE BASELINE MODEL
-      BaselineModel model = new BaselineModel();
-      
-      model.train(trainData);
-      System.out.println("Training Complete.");
-      
-      model.test(testData, options.get("-outfile"));
-      System.out.println("Testing Complete.");
-    }
-    else {
-      System.err.println("ERROR: Model type not recognized");
-    }
-  }
-  
-  
-  /**
-   * Print all the runtime options from the options map
-   */
-  public static void printOptions(Map<String, String> options) {
-    System.out.println("NER Run Parameters:");
-    for (Map.Entry<String, String> entry: options.entrySet()) {
-      System.out.printf("  %-12s: %s%n", entry.getKey(), entry.getValue());
-    }
-    System.out.println();
-  }
-  
-  
-  /**
-   * Convert the modelName string into an instance of the model class
-   */
-  public static NERModel getModelClass(String modelName) {
-    NERModel model;
-    try {
-      Class<?> modelClass = Class.forName(modelName);
-      model = (NERModel) modelClass.newInstance();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-    return model;
+    model.train(trainData);
+    model.test(testData, "../ner.out");
   }
 }
