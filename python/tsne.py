f6143cf329f2c1f255eccea7013c14d874951067
Patrick Manion <patrickjmanion@gmail.com>
2015-12-05 02:03:52 -0600

    Final report

--- python/tsne.py
+++ python/tsne.py
@@ -61,15 +61,22 @@ lookup = np.vectorize(lambda(x): id_map[x])
 word_ids = np.arange(orig_wordvec.shape[0])
 all_entities = lookup(word_ids)
 
-o_entities = (all_entities[:] == entity_list.index('O'))
-o_sample = np.random.choice([True,False],
-                              size=orig_wordvec.shape[0],
-                              p=[0.005, 1-0.005])
-non_o_sample = np.random.choice([True,False],
-                                size=orig_wordvec.shape[0],
-                                p=[0.05, 1-0.05])
+#o_entities = (all_entities[:] == entity_list.index('O'))
+#o_sample = np.random.choice([True,False],
+#                              size=orig_wordvec.shape[0],
+#                              p=[0.005, 1-0.005])
+#non_o_sample = np.random.choice([True,False],
+#                                size=orig_wordvec.shape[0],
+#                                p=[0.05, 1-0.05])
+#row_sample = (o_entities * o_sample) + (~o_entities * non_o_sample)
 
-row_sample = (o_entities * o_sample) + (~o_entities * non_o_sample)
+row_sample = np.random.choice([True,False],
+                               size=orig_wordvec.shape[0],
+                               p=[0.05, 1-0.05])
+o_entities = (all_entities[:] == entity_list.index('O'))
+
+row_sample = row_sample * ~o_entities
+
 
 # Downsample the rows
 slim_orig = orig_wordvec[row_sample,:]
@@ -77,7 +84,7 @@ del(orig_wordvec)
 slim_new  = new_wordvec[row_sample,:]
 del(new_wordvec)
 slim_entities = all_entities[row_sample]
-del(all_entites)
+del(all_entities)
 
 
 
@@ -95,31 +102,3 @@ plt.scatter(new_tsne[:,0],  new_tsne[:,1], c=slim_entities)
 plt.xlabel('t-SNE Dimension 1')
 plt.ylabel('t-SNE Dimension 2')
 plt.title('t-SNE Projection AFTER Training')
-
-def plot(X, title=None):
-    x_min, x_max = np.min(X, 0), np.max(X, 0)
-    X = (X - x_min) / (x_max - x_min)
-
-    plt.figure()
-    ax = plt.subplot(111)
-    for i in range(X.shape[0]):
-        plt.text(X[i, 0], X[i, 1], str(digits.target[i]),
-                 color=plt.cm.Set1(y[i] / 10.),
-                 fontdict={'weight': 'bold', 'size': 9})
-
-    if hasattr(offsetbox, 'AnnotationBbox'):
-        # only print thumbnails with matplotlib > 1.0
-        shown_images = np.array([[1., 1.]])  # just something big
-        for i in range(digits.data.shape[0]):
-            dist = np.sum((X[i] - shown_images) ** 2, 1)
-            if np.min(dist) < 4e-3:
-                # don't show points that are too close
-                continue
-            shown_images = np.r_[shown_images, [X[i]]]
-            imagebox = offsetbox.AnnotationBbox(
-                offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r),
-                X[i])
-            ax.add_artist(imagebox)
-    plt.xticks([]), plt.yticks([])
-    if title is not None:
-        plt.title(title)
